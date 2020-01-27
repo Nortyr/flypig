53c57da7db7488064ef3e36ec0d0a3f49b9a76ef
Nortyr <321785796@qq.com>
2020-01-27 21:04:54 +0800

    feat:init

---
+++ flypig/src/main/java/com/uino/flypig/FlypigApplication.java
@@ -0,0 +1,13 @@
+package com.uino.flypig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlypigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlypigApplication.class, args);
+    }
+
+}
